88c50ddc7bb09ca9cbc346a6960cd7d04cebfa81
James Ferguson <ncjamesferguson@gmail.com>
2018-05-11 21:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = new Date(startDate);
   }
 
-  yearsExpereienceFromBeginningOf(year){
+  yearsExperienceFromBeginningOf(year){
     return year - this.startDate
   }
 }
