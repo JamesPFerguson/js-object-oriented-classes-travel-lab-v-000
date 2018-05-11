2ca312bc1c3d2f5ff6bf8562e8eb4b0c8bf05b27
James Ferguson <ncjamesferguson@gmail.com>
2018-05-11 21:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ class Driver {
 }
 
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
 class Route {
-
   constructor(beginningLocation, endingLocation) {
-   this.beginningLocation = beginningLocation;
-   this.endingLocation = endingLocation;
- };
-
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
 
   blocksTravelled() {
-   let distNorthSouth = Math.abs(parseInt(this.beginningLocation.vertical) - parseInt(this.endingLocation.vertical));
-   let distEastWest = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
-   return distNorthSouth + distEastWest;
- };
+    let distNorthSouth = Math.abs(parseInt(this.beginningLocation.vertical) - parseInt(this.endingLocation.vertical));
+    let distEastWest = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    return distNorthSouth + distEastWest;
+  };
 
- estimatedTime(peak) {
-   if (peak) {
-     return this.blocksTravelled()/2;
-   } else {
-     return this.blocksTravelled()/3;
-   }
- }
-
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    }
+  }
 }
