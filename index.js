af6d0d9734f42a3e8a4fa6c51660eb93eb7022ee
James Ferguson <ncjamesferguson@gmail.com>
2018-05-11 21:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ class Driver {
 
 
 class Route {
+  
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
+
+  
 }
+
